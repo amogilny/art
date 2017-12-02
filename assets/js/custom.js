c8518039f660ead3933f3ac6b52eb7ff5bc8bc3a
Alexey <>
2017-12-02 13:01:27 +0300

    смена цвета

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,11 +3,43 @@ function declOfNum(n, titles) {
 }
 
 // Countdown
-$('#clock').countdown('2017/12/31').on('update.countdown', function(event) {
+$('#clock').countdown('2019/01/01').on('update.countdown', function(event) {
     var $this = $(this).html(event.strftime(''
     + '<div><span>%-w</span>' + declOfNum(event.strftime('%-w'), ['неделя', 'недели', 'недель']) + '</div>'
     + '<div><span>%-d</span>' + declOfNum(event.strftime('%-d'), ['день', 'дня', 'дней']) + '</div>'
     + '<div><span>%H</span>' + declOfNum(event.strftime('%-H'), ['час', 'часа', 'часов']) + '</div>'
     + '<div><span>%M</span>' + declOfNum(event.strftime('%-M'), ['минута', 'минуты', 'минут']) + '</div>'
     ));
+});
+
+/*
+ * Replace all SVG images with inline SVG
+ */
+$('img.svg').each(function(){
+    var $img = $(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
 });
